b3e3ce79adc1f13f1073ff8e462b62fba6714929
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-04-24 18:04:18 +0200

    create xcode project

---
+++ Step tracker/Step_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_trackerApp.swift
+//  Step tracker
+//
+//  Created by olivier geiger on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
